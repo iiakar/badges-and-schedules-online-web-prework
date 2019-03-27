5434634d2e683f731e7fa9ac99f715325383bde3
Ibrahim  Akar <ibsakar@gmail.com>
2019-03-27 15:12:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,7 @@ def batch_badge_creator(names)
 
 def assign_rooms(speakers)
   speakers.collect.each_with_index do |speaker,index|
-    "Hello, #{speaker}"!
+    "Hello, #{speaker}"! You will be assign_rooms
 
 You have rooms 1-7.
 
