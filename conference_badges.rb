07961d808b0a0aaf341e66d39843ada7981bac12
Ibrahim  Akar <ibsakar@gmail.com>
2019-03-27 15:25:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,7 +21,7 @@ def printer(attendees)
   batch_badge_creator(attendees).each do (attendee)
   puts attendee
 end 
-assign_rooms(attendees)
+assign_rooms(attendees).each do 
 
 
 You have rooms 1-7.
